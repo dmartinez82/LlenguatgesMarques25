2f5afc50097bca057d3951a69363c0adef8ff74a
Daniel Martínez <dmartinez@esliceu.com>
2026-01-20 17:12:07 +0100

    comprovacions i modificació DOM

--- js/practicaJS.js
+++ js/practicaJS.js
@@ -51,41 +51,29 @@ function compruebaEdad() {
 }
 
 function afegir(){
-    let interesUsuari = DOM.intereses.value;
-    // interesos[interesos.length] = interesUsuari;
-    interesos.push(interesUsuari);
-    mostraResultats();
+    let interesUsuari = DOM.intereses.value.trim();
+    const indexTrobat = interesos.findIndex(function (element) {
+        if (element.toUpperCase() == interesUsuari.toUpperCase())
+            return true;
+        else 
+            return false;
+    });
+
+    if (interesUsuari && indexTrobat == -1){
+        interesos.push(interesUsuari);
+        mostraResultats();
+    }
+    
 }
 
 function mostraResultats(){
-    // DOM.resultatInteresos.textContent = interesos;
+    DOM.resultatInteresos.innerHTML = "";
 
-    DOM.resultatInteresos.textContent = "";
-
-    let i = 0;
-    while(i< interesos.length){
-        DOM.resultatInteresos.textContent += interesos[i] + ",";
-        i++;
-    }
-
-    DOM.resultatInteresos.textContent = "";
-
-    for(let i = 0; i< interesos.length; i++){
-        DOM.resultatInteresos.textContent += interesos[i] + ";";
-    }
-
-    DOM.resultatInteresos.textContent = "";
-
+    let resultatHTML = "<ul>";
     for(let interes of interesos){
-        DOM.resultatInteresos.textContent += interes + "/";
+        resultatHTML += "<li>" + interes + "</li>";
     }
+    resultatHTML += "</ul>";
 
-    DOM.resultatInteresos.textContent = "";
-
-    interesos.forEach(function(element){
-        DOM.resultatInteresos.textContent += element + "-";
-    });
-
-
-
+    DOM.resultatInteresos.innerHTML = resultatHTML;
 }
